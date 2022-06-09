847e294902262b055bc593bab99693c15857a893
ivansimplicio <ivansimplicio@outlook.com>
2022-06-09 20:08:02 -0300

    feat: customizing seeders execution order

---
+++ database/seeders/MainSeeder/Index.ts
@@ -0,0 +1,15 @@
+import Application from '@ioc:Adonis/Core/Application'
+import BaseSeeder from '@ioc:Adonis/Lucid/Seeder'
+
+export default class IndexSeeder extends BaseSeeder {
+  private async runSeeder(seeder: { default: typeof BaseSeeder }) {
+    if (seeder.default.developmentOnly && !Application.inDev) {
+      return
+    }
+    await new seeder.default(this.client).run()
+  }
+  public async run() {
+    await this.runSeeder(await import('../Category'))
+    await this.runSeeder(await import('../Product'))
+  }
+}
