c3e287847a450f778f0c4729db31c98c2964f64f
ivansimplicio <ivansimplicio@outlook.com>
2022-06-11 09:31:32 -0300

    feat: created the User model

---
+++ app/Models/User.ts
@@ -0,0 +1,30 @@
+import { DateTime } from 'luxon'
+import { BaseModel, column } from '@ioc:Adonis/Lucid/Orm'
+
+export default class User extends BaseModel {
+  public static table = 'users'
+
+  @column({ isPrimary: true })
+  public id: number
+
+  @column()
+  public name: string
+
+  @column()
+  public email: string
+
+  @column({ serializeAs: null })
+  public password: string
+
+  @column()
+  public cpf: string
+
+  @column({ columnName: 'phone_number' })
+  public phoneNumber: string
+
+  @column.dateTime({ autoCreate: true })
+  public createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  public updatedAt: DateTime
+}
