d0a3ae7c60a4e5ba0823871205a06fd214e370e4
ivansimplicio <ivansimplicio@outlook.com>
2022-06-11 08:59:17 -0300

    feat: created the migration to the users table

---
+++ database/migrations/1654948354816_create_table_users.ts
@@ -0,0 +1,22 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'users'
+
+  public async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id').primary()
+      table.string('name', 100).notNullable()
+      table.string('email', 100).notNullable().unique()
+      table.string('password', 255).notNullable()
+      table.string('cpf', 11).notNullable().unique()
+      table.string('phone_number', 20)
+      table.timestamp('created_at', { useTz: true })
+      table.timestamp('updated_at', { useTz: true })
+    })
+  }
+
+  public async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}
