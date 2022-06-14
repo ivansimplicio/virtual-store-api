b31a1ed2c719500fb11c296c7a261779baa390fd
ivansimplicio <ivansimplicio@outlook.com>
2022-06-14 19:55:34 -0300

    feat: created the seeder for Order

--- database/seeders/MainSeeder/Index.ts
+++ database/seeders/MainSeeder/Index.ts
@@ -15,5 +15,6 @@ export default class IndexSeeder extends BaseSeeder {
     await this.runSeeder(await import('../User'))
     await this.runSeeder(await import('../Address'))
     await this.runSeeder(await import('../DeliveryAddress'))
+    await this.runSeeder(await import('../Order'))
   }
 }
---
+++ database/seeders/Order.ts
@@ -0,0 +1,54 @@
+import BaseSeeder from '@ioc:Adonis/Lucid/Seeder'
+import Order from 'App/Models/Order'
+
+export default class OrderSeeder extends BaseSeeder {
+  public async run() {
+    const uniqueKey = 'id'
+    const orders = await Order.updateOrCreateMany(uniqueKey, [
+      {
+        id: 1,
+        amount: 7998,
+        addressId: 1,
+        userId: 2,
+      },
+      {
+        id: 2,
+        amount: 1495.24,
+        addressId: 2,
+        userId: 3,
+      },
+    ])
+    await orders[0].related('items').createMany([
+      {
+        id: 1,
+        quantity: 2,
+        discount: 0,
+        amount: 7998,
+        productId: 1,
+      },
+    ])
+    await orders[1].related('items').createMany([
+      {
+        id: 2,
+        quantity: 1,
+        discount: 0,
+        amount: 89.91,
+        productId: 3,
+      },
+      {
+        id: 3,
+        quantity: 1,
+        discount: 0,
+        amount: 1015.43,
+        productId: 4,
+      },
+      {
+        id: 4,
+        quantity: 1,
+        discount: 0,
+        amount: 389.9,
+        productId: 5,
+      },
+    ])
+  }
+}
