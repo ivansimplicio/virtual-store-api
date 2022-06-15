29038b2ed02c80d25556446ae47f692abdeb6986
ivansimplicio <ivansimplicio@outlook.com>
2022-06-15 08:26:27 -0300

    fix: change in orders table

---
+++ database/migrations/1655289979542_alter_table_orders.ts
@@ -0,0 +1,11 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'orders'
+
+  public async up() {
+    this.schema.alterTable(this.tableName, (table) => {
+      table.dropForeign('address_id')
+    })
+  }
+}
