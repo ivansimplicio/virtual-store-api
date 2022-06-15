72a0ea77912d2a5a3bafdee8446db6b4f8e31837
ivansimplicio <ivansimplicio@outlook.com>
2022-06-15 15:24:19 -0300

    feat: created the router for Order

---
+++ app/Modules/Order/routes.ts
@@ -0,0 +1,7 @@
+import Route from '@ioc:Adonis/Core/Route'
+
+Route.group(() => {
+  Route.get('/', 'OrdersController.index')
+  Route.post('/', 'OrdersController.store')
+  Route.get('/:id', 'OrdersController.show')
+}).prefix('orders')
