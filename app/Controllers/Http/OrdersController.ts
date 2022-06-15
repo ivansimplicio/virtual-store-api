261603f6137df018b8e23b74ca86b9f61cdccd4c
ivansimplicio <ivansimplicio@outlook.com>
2022-06-15 15:24:08 -0300

    feat: created the controller for Order

---
+++ app/Controllers/Http/OrdersController.ts
@@ -0,0 +1,25 @@
+import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import OrdersService from 'App/Services/OrdersService'
+
+export default class OrdersController {
+  public async index({ response }: HttpContextContract) {
+    const orders = await OrdersService.findAll()
+    return response.ok({ orders })
+  }
+
+  public async show({ request, response }: HttpContextContract) {
+    const orderId = request.param('id')
+    const order = await OrdersService.find(orderId)
+    if (!order) {
+      return response.notFound()
+    }
+    return response.ok({ order })
+  }
+
+  public async store({ request, response }: HttpContextContract) {
+    const USER_ID = 2
+    const payload = request.only(['addressId', 'items'])
+    const order = await OrdersService.store(USER_ID, payload)
+    return response.ok({ order })
+  }
+}
