7c41e23542c3954065a685840ff8f0c873faa2e2
ivansimplicio <ivansimplicio@outlook.com>
2022-06-22 10:35:40 -0300

    feat: sending customer welcome email

---
+++ app/Mailers/WelcomeClient.ts
@@ -0,0 +1,16 @@
+import Env from '@ioc:Adonis/Core/Env'
+import { BaseMailer, MessageContract } from '@ioc:Adonis/Addons/Mail'
+
+export default class WelcomeClient extends BaseMailer {
+  constructor(private name: string, private email: string) {
+    super()
+  }
+
+  public prepare(message: MessageContract) {
+    message
+      .subject(`VS: Bem-vindo(a), ${this.name}`)
+      .from(Env.get('SMTP_EMAIL_SENDER'))
+      .to(this.email)
+      .htmlView('emails/welcome_client', { payload: { name: this.name, email: this.email } })
+  }
+}
--- app/Services/UsersService.ts
+++ app/Services/UsersService.ts
@@ -1,4 +1,5 @@
 import Roles from 'App/Enums/Roles'
+import WelcomeClient from 'App/Mailers/WelcomeClient'
 import User from 'App/Models/User'
 
 type UserInsert = {
@@ -45,6 +46,7 @@ class UsersService {
     await createdUser.related('addresses').create(address)
     await createdUser.related('roles').attach([Roles.CLIENT])
     await this.loadUserRelationships(createdUser)
+    await new WelcomeClient(createdUser.name, createdUser.email).sendLater()
     return createdUser
   }
 
