facc39100e446c2755944d323b531e785f4cd4f2
ivansimplicio <ivansimplicio@outlook.com>
2022-06-23 19:13:12 -0300

    refactor: order table creation migration change

--- database/migrations/1655226963798_create_table_orders.ts
+++ database/migrations/1655226963798_create_table_orders.ts
@@ -7,7 +7,7 @@ export default class extends BaseSchema {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id').primary()
       table.double('amount').notNullable()
-      table.integer('user_id').unsigned().references('id').inTable('users').onDelete('CASCADE')
+      table.integer('user_id').unsigned().references('id').inTable('users').onDelete('SET NULL')
       table.integer('address_id').unsigned().references('id').inTable('delivery_addresses')
       table.timestamp('created_at', { useTz: true })
       table.timestamp('updated_at', { useTz: true })
