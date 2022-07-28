cff42bcb7c710e7b92d337312a0579980952feee
ivansimplicio <ivansimplicio@outlook.com>
2022-07-28 14:48:50 -0300

    refactor: exceptions

--- app/Exceptions/InvalidAddressException.ts
+++ app/Exceptions/InvalidAddressException.ts
@@ -1,6 +1,6 @@
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import { Exception } from '@adonisjs/core/build/standalone'
-import BadRequest from './Errors/BadRequest'
+import UnprocessableEntity from './Errors/UnprocessableEntity'
 
 export default class InvalidAddressException extends Exception {
   constructor() {
@@ -8,7 +8,7 @@ export default class InvalidAddressException extends Exception {
   }
 
   public async handle(error: this, ctx: HttpContextContract) {
-    const badRequest = new BadRequest(error.message)
-    ctx.response.status(badRequest.status).send(badRequest)
+    const unprocessableEntity = new UnprocessableEntity(error.message)
+    ctx.response.status(unprocessableEntity.status).send(unprocessableEntity)
   }
 }
--- app/Exceptions/InvalidPasswordException.ts
+++ app/Exceptions/InvalidPasswordException.ts
@@ -1,6 +1,6 @@
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
-import BadRequest from 'App/Exceptions/Errors/BadRequest'
 import { Exception } from '@adonisjs/core/build/standalone'
+import UnprocessableEntity from './Errors/UnprocessableEntity'
 
 export default class InvalidPasswordException extends Exception {
   constructor() {
@@ -8,7 +8,7 @@ export default class InvalidPasswordException extends Exception {
   }
 
   public async handle(error: this, ctx: HttpContextContract) {
-    const badRequest = new BadRequest(error.message)
-    ctx.response.status(badRequest.status).send(badRequest)
+    const unprocessableEntity = new UnprocessableEntity(error.message)
+    ctx.response.status(unprocessableEntity.status).send(unprocessableEntity)
   }
 }
